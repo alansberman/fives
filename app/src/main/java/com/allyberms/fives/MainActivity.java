6419bad3e5a9b00ebe8be4d796bd12b2be708e25
Alan Berman <alan.berman.92@gmail.com>
2019-07-21 20:04:26 +0200

    moved files and kinda working

--- app/src/main/java/com/allyberms/fives/MainActivity.java
+++ app/src/main/java/com/allyberms/fives/MainActivity.java
@@ -1,5 +1,6 @@
 package com.allyberms.fives;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,27 +28,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         startGame();
+        System.out.println(chosenWord);
+        guesses = new HashMap<String,Integer>();
     }
 
     public void buttonClick(View button) {
         EditText guessBox = findViewById(R.id.word_guess);
         String entered_guess = guessBox.getText().toString().toLowerCase();
         guess = entered_guess;
-        scoreWord();
+        scoreWord(guess);
         getGuessHistory();
     }
 
 
-    public void addToGuesses() {
+    public void addToGuesses(String guess,Integer wordScore) {
         guesses.put(guess,wordScore);
     }
 
     public void startGame(){
 
         if (wordList==null) {
-            setUpWordList();
+            try {
+                setUpWordList();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        selectWord();
+        chosenWord = selectWord();
 
     }
 
@@ -57,39 +64,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Set up the word list
-    public void setUpWordList(){
+    public void setUpWordList() throws IOException {
         wordList = new ArrayList<String>();
+
         try {
-            BufferedReader input = new BufferedReader(new FileReader("../fives_words.txt"));
+            AssetManager assetManager = getAssets();
+            String[] files = assetManager.list("");
+            InputStream input = assetManager.open(files[0]);
+            BufferedReader inputs =  new BufferedReader(new InputStreamReader(input, "UTF-8"));
             String line;
             String word;
             StringBuffer buffer = new StringBuffer();
-            while ((line= input.readLine())!=null) {
+            while ((line= inputs.readLine())!=null) {
                 buffer.append(line);
                 word = buffer.toString();
                 wordList.add(word);
+                buffer.setLength(0);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+           } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public String getGuessHistory(){
-        String guessHistory="";
+    public ArrayList<String> getGuessHistory(){
+        ArrayList<String> guessHistory= new ArrayList<String>();
 
         //iterate through guesses hashmap
         for (Map.Entry<String,Integer> entry : guesses.entrySet()) {
-            guessHistory += entry.getKey()+" : "+entry.getValue()+"/n";
+            guessHistory.add(entry.getKey()+" : "+entry.getValue().toString());
         }
 
         return guessHistory;
     }
 
     //Score a word
-    public void scoreWord() {
+    public void scoreWord(String guess) {
         reset();
+        System.out.println(guess+chosenWord);
         for (int c = 0; c < guess.length(); c++)
         {
             // if char in word
@@ -101,20 +114,22 @@ public class MainActivity extends AppCompatActivity {
         //check if won game
         if (wordScore==5)
         {
-            boolean outcome = checkWord();
+            boolean outcome = checkWord(guess);
             if (outcome){
                 wonGame();
             }
         }
-        addToGuesses();
+        addToGuesses(guess,wordScore);
+
     }
 
     public void wonGame() {
         //to do
+        System.out.println("well done!");
     }
 
     // select a word from the word list
-    public void selectWord()
+    public String selectWord()
     {
         if (!wordList.isEmpty()) {
 
@@ -122,14 +137,14 @@ public class MainActivity extends AppCompatActivity {
             Random randomGenerator = new Random();
             int randomPosition = randomGenerator.nextInt(wordListLength);
             chosenWord = wordList.get(randomPosition);
+            System.out.println(chosenWord);
         }
+        return chosenWord;
     }
 
-    public boolean checkWord() {
+    public boolean checkWord(String guess) {
 
-        if (guess.equalsIgnoreCase(chosenWord)){
-            return true;
-        }
-        return false;
+        return guess.equalsIgnoreCase(chosenWord);
+
     }
 }
